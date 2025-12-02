3dc98fe04da96f83cb9794125277ef080de02973
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-12-02 14:11:31 +0100

    add: E027

--- src/main/java/io/spring/graphql/SecurityUtil.java
+++ src/main/java/io/spring/graphql/SecurityUtil.java
@@ -7,13 +7,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtil {
+
   public static Optional<User> getCurrentUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    if (authentication instanceof AnonymousAuthenticationToken
-        || authentication.getPrincipal() == null) {
-      return Optional.empty();
-    }
     io.spring.core.user.User currentUser = (io.spring.core.user.User) authentication.getPrincipal();
     return Optional.of(currentUser);
   }
+
 }
