3d94e757f9c5d70bac483edb4fa9b6e5d53ef6ef
G1t-Happens <44774729+G1t-Happens@users.noreply.github.com>
2025-12-02 14:51:23 +0100

    add: E030

--- src/main/java/io/spring/api/ArticlesApi.java
+++ src/main/java/io/spring/api/ArticlesApi.java
@@ -26,8 +26,7 @@ public class ArticlesApi {
   private ArticleQueryService articleQueryService;
 
   @PostMapping
-  public ResponseEntity createArticle(
-      @Valid @RequestBody NewArticleParam newArticleParam, @AuthenticationPrincipal User user) {
+  public ResponseEntity createArticle(@RequestBody NewArticleParam newArticleParam, @AuthenticationPrincipal User user) {
     Article article = articleCommandService.createArticle(newArticleParam, user);
     return ResponseEntity.ok(
         new HashMap<String, Object>() {
